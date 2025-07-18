092883ea144e79dcc8eca10e917bcca2a57f7169
sudo-mnorton <michael_james_norton@hotmail.com>
2025-07-18 15:53:14 +0100

    Bug testing: fix port issue

--- app/app.py
+++ app/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8888))
+    app.run(host="0.0.0.0", port=port)
