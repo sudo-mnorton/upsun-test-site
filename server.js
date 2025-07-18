8ce8b6019007569b641bd91ab147f021cf5167e0
sudo-mnorton <michael_james_norton@hotmail.com>
2025-07-18 12:08:15 +0100

    Change port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
-const port = 80;
+const port = 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
